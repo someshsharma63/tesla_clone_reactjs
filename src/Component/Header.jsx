d0bda002277b4bfc106d8438534d138044ed3060
Somesh <someshdevera98@gmail.com>
2025-05-20 10:31:51 +0530

    add cross icon to close the sidebar

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -1,43 +1,31 @@
 import React, { useState } from "react";
 import Logo from "../Images/logo.svg";
+
 function Header() {
   const [sidemenu, setSidemenu] = useState(false);
+
   return (
     <>
       <header>
         <div className="logo">
-          <img src={Logo} alt="" />
+          <img src={Logo} alt="Logo" />
         </div>
+
         <div className="menus_center">
           <ul>
-            <li>
-              <a href="">Model S</a>
-            </li>
-            <li>
-              <a href="">Model 3</a>
-            </li>
-            <li>
-              <a href="">Model X</a>
-            </li>
-            <li>
-              <a href="">Model Y</a>
-            </li>
-            <li>
-              <a href="">Solar Roof</a>
-            </li>
-            <li>
-              <a href="">Solar panel</a>
-            </li>
+            <li><a href="">Model S</a></li>
+            <li><a href="">Model 3</a></li>
+            <li><a href="">Model X</a></li>
+            <li><a href="">Model Y</a></li>
+            <li><a href="">Solar Roof</a></li>
+            <li><a href="">Solar Panel</a></li>
           </ul>
         </div>
+
         <div className="menus_center">
           <ul>
-            <li>
-              <a href="">Shop</a>
-            </li>
-            <li>
-              <a href="">Account</a>
-            </li>
+            <li><a href="">Shop</a></li>
+            <li><a href="">Account</a></li>
             <li
               onClick={(e) => {
                 e.preventDefault();
@@ -50,17 +38,26 @@ function Header() {
         </div>
 
         <ul className="sidemenu" style={{ right: sidemenu ? "0" : "-250px" }}>
-          <li>kdng</li>
-          <li>kdng</li>
-          <li>kdng</li>
-          <li>kdng</li>
-          <li>kdng</li>
-          <li>kdng</li>
-          <li>kdng</li>
-          <li>kdng</li>
-          <li>kdng</li>
-          <li>kdng</li>
-          <li>kdng</li>
+          {/* Cross/Close Icon */}
+          <li
+            style={{
+              textAlign: "right",
+              cursor: "pointer",
+              fontWeight: "bold",
+              fontSize: "20px",
+              padding: "10px",
+            }}
+            onClick={() => setSidemenu(false)}
+          >
+            &times; {/* This is the × character */}
+          </li>
+
+          {/* Your menu items */}
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Service</li>
+          <li>Support</li>
         </ul>
       </header>
     </>
